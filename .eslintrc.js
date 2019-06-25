accd806d9bc822a8d81641ddbcd5eb613b4ee7c8
rafaelsene01 <rafaelssene@gmail.com>
2019-06-25 13:34:38 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,6 @@ module.exports = {
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", {"argsIgnorePattern": "nex"}]
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
   },
 };
