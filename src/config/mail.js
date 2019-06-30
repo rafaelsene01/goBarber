426c9192d722118e63b94c223606b02de82edf04
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-06-30 17:17:46 -0300

    sending email to the provider when a client cancels a schedule

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fd8c3ff5920978',
+    pass: '1409fcf94baa7b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com >',
+  },
+};
