5d4ba7de8e82b7146e949cf0770dffea33d5e512
Rafael Sene Alves Penha <rafaelssene@gmail.com>
2019-07-01 11:01:50 -0300

    fixing bug in models appointment

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -5,7 +5,7 @@ class Appointment extends Model {
     super.init(
       {
         date: Sequelize.DATE,
-        canceled_at: Sequelize.BOOLEAN,
+        canceled_at: Sequelize.DATE,
       },
       { sequelize }
     );
