408bfb326c0abd833e274576c6fd051771aa12b7
Rafael Sene <rafaelssene@gmail.com>
2019-07-16 17:37:55 -0300

    validations

--- frontend/src/pages/SingUp/index.js
+++ frontend/src/pages/SingUp/index.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Nome obrigatoria'),
+  email: Yup.string()
+    .email('Insira um e-mail valido')
+    .required('O e-mail e obrigatorio'),
+  password: Yup.string()
+    .min(6, 'Minimo 6 caracteres')
+    .required('Senha obrigatoria'),
+});
 
 export default function SingUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="gobarber" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Criar conta</button>
+
+        <Link to="/">Ja tenho login</Link>
+      </Form>
+    </>
+  );
 }
