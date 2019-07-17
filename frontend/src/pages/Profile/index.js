52d4667fa9efc046c92801374e80027b8cb68c13
Rafael Sene <rafaelssene@gmail.com>
2019-07-17 13:42:40 -0300

    Profile teste

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu endereco de E-mail" />
+
+        <br />
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Conformacao de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+    </Container>
+  );
 }
