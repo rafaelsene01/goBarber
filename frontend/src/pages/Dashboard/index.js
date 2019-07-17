b689f26125bfedbda8c3f35f54e5b859ca3354d2
Rafael Sene <rafaelssene@gmail.com>
2019-07-17 15:47:41 -0300

    styles dashboard

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function() {
-  api.get('appointments');
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>31 Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
 
-  return <div />;
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Nome</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Open</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Nome</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Nome</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
