15abec036bb95c8da7092adb29063400062e6819
Rafael Sene <rafaelssene@gmail.com>
2019-07-17 14:04:21 -0300

    config Profile and styles

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
     <Container>
-      <Form>
+      <Form initialData={profile}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" placeholder="Seu endereco de E-mail" />
 
-        <br />
+        <hr />
         <Input
           name="oldPassword"
           type="password"
@@ -25,6 +30,7 @@ export default function Profile() {
 
         <button type="submit">Atualizar perfil</button>
       </Form>
+      <button type="submit">Sair do GoBarber</button>
     </Container>
   );
 }
