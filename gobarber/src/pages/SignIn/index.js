6ac921ffc510a4db68365159e610c6bc06a9f137
Rafael Sene <rafaelssene@gmail.com>
2019-07-21 17:09:44 -0300

    global components

--- gobarber/src/pages/SignIn/index.js
+++ gobarber/src/pages/SignIn/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import Backgound from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Backgound>
       <Text>aaa</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+      <Button>Entrar</Button>
     </Backgound>
   );
 }
