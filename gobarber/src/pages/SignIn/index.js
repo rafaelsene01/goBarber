71c96d24e7f094ef0b9a70ac3a79d33656d809c7
Rafael Sene <rafaelssene@gmail.com>
2019-07-21 16:27:54 -0300

    config background

--- gobarber/src/pages/SignIn/index.js
+++ gobarber/src/pages/SignIn/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Backgound from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <Text>aaa</Text>;
+  return (
+    <Backgound>
+      <Text>aaa</Text>
+    </Backgound>
+  );
 }
--- gobarber/src/pages/SignUp/index.js
+++ gobarber/src/pages/SignUp/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import Backgound from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <View />;
+  return (
+    <Backgound>
+      <Text>aaa</Text>
+    </Backgound>
+  );
 }
