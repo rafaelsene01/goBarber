1d3475c85ec0c0d03dd745133eeae97514b3405d
Rafael Sene <rafaelssene@gmail.com>
2019-07-22 12:57:56 -0300

    config SignIn

--- gobarber/src/pages/SignIn/index.js
+++ gobarber/src/pages/SignIn/index.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Backgound from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
-// import { Container } from './styles';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 export default function SignIn() {
   return (
     <Backgound>
-      <Text>aaa</Text>
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="mail-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Backgound>
   );
 }
