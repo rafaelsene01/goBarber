d2dd356c9d639972e29a85f6a084a9af244b5b90
Rafael Sene <rafaelssene@gmail.com>
2019-07-22 16:26:45 -0300

    config SignUp

--- gobarber/src/pages/SignUp/index.js
+++ gobarber/src/pages/SignUp/index.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Backgound from '~/components/Background';
-// import { Container } from './styles';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
   return (
     <Backgound>
-      <Text>aaa</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+          <FormInput
+            icon="mail-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Logar</SignLinkText>
+        </SignLink>
+      </Container>
     </Backgound>
   );
 }
