e946a2feba7945fb6ae3a6e1e46d0f49f4b7da9b
Rafael Sene <rafaelssene@gmail.com>
2019-07-23 16:33:48 -0300

    seeking from api

--- gobarber/src/pages/Dashboard/index.js
+++ gobarber/src/pages/Dashboard/index.js
@@ -1,23 +1,50 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { async } from 'rxjs/internal/scheduler/async';
+import api from '~/services/api';
 
 import Backgound from '~/components/Background';
 import Appointment from '~/components/Appointment';
 import { Container, Title, List } from './styles';
 
-const data = [1, 2, 3, 4, 5];
-
 export default function Dashboard() {
+  const [appointments, setAppointments] = useState([]);
+
+  useEffect(() => {
+    async function loadAppointments() {
+      const response = await api.get('appointments');
+
+      setAppointments(response.data);
+    }
+
+    loadAppointments();
+  }, []);
+
+  async function handleCancel(id) {
+    try {
+      const response = await api.delete(`appointments/${id}`);
+
+      setAppointments(
+        appointments.map(appo =>
+          appo.id === id
+            ? { ...appo, canceled_at: response.data.canceled_at }
+            : appo
+        )
+      );
+    } catch (error) {}
+  }
+
   return (
     <Backgound>
       <Container>
         <Title>Agendamentos</Title>
 
         <List
-          data={data}
-          keyExtractor={item => String(item)}
-          renderItem={({ item }) => <Appointment data={item} />}
+          data={appointments}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => (
+            <Appointment onCancel={() => handleCancel(item.id)} data={item} />
+          )}
         />
       </Container>
     </Backgound>
