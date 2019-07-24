5fb01bfe06da4892f6e8c1822e4715349a7b9642
Rafael Sene <rafaelssene@gmail.com>
2019-07-24 00:01:41 -0300

    correcting backend, availableController

--- backend/src/app/controllers/AvailableController.js
+++ backend/src/app/controllers/AvailableController.js
@@ -44,7 +44,7 @@ class AvailableController {
       '18:00',
     ];
 
-    const avaiable = schedule.map(time => {
+    const available = schedule.map(time => {
       const [hour, minute] = time.split(':');
       const value = setSeconds(
         setMinutes(setHours(searchDate, hour), minute),
@@ -54,13 +54,13 @@ class AvailableController {
       return {
         time,
         value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
-        avaiable:
+        available:
           isAfter(value, new Date()) &&
           !appointment.find(a => format(a.date, 'HH:mm') === time),
       };
     });
 
-    return res.json(avaiable);
+    return res.json(available);
   }
 }
 
