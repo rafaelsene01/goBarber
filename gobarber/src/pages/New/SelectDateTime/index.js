b8796350422d2a04836f3925157246fa4125d181
Rafael Sene <rafaelssene@gmail.com>
2019-07-23 21:35:28 -0300

    provider listing

--- gobarber/src/pages/New/SelectDateTime/index.js
+++ gobarber/src/pages/New/SelectDateTime/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SelectDateTime() {
-  return <View />;
+  return <Background />;
 }
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+  title: 'Selecione o horario',
+  headerLeft: () => (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('SelectProvider');
+      }}
+    >
+      <Icon name="arrow-back" size={20} color="#FFF" />
+    </TouchableOpacity>
+  ),
+});
